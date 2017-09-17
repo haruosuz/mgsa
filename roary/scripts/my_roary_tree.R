f8f48665b26983b4366e7fc3fe20c06f4da620b8
haruosuz <haruosuz@yahoo.co.jp>
2017-09-17 19:03:18 +0900

    update roary/scripts/my_roary_tree.R

--- roary/scripts/my_roary_tree.R
+++ roary/scripts/my_roary_tree.R
@@ -3,9 +3,10 @@
 #setwd("~/projects/mgsa/roary/2016-06-21") # i 50
 setwd("~/projects/jc923/nckuh21/2017-09-17")
 
-system("find data -name '*.gbk' | xargs grep 'ORGANISM' | sort -u | grep -v 'phage' | sed 's/ ORGANISM  //; s#.*/##' | perl -pe 's/.gbk:/.gbk\t/;' > my.file.ORGANISM.txt; find data -name '*.gbk' | xargs grep '/strain=' | sort -u | sed 's# */strain=\"##; s#\"##g; s#.*/##' | perl -pe 's/.gbk:/.gbk\t/;' > my.file.strain.txt; join -1 1 -2 1 -a 1 -t "$(printf '\011')" my.file.ORGANISM.txt my.file.strain.txt > my.file.name.txt")
+#system("find data -name '*.gbk' | xargs grep 'ORGANISM' | sort -u | grep -v 'phage' | sed 's/ ORGANISM  //; s#.*/##' | perl -pe 's/.gbk:/.gbk\t/;' > my.file.ORGANISM.txt; find data -name '*.gbk' | xargs grep '/strain=' | sort -u | sed 's# */strain=\"##; s#\"##g; s#.*/##' | perl -pe 's/.gbk:/.gbk\t/;' > my.file.strain.txt; join -1 1 -2 1 -a 1 -t "$(printf '\011')" my.file.ORGANISM.txt my.file.strain.txt > my.file.name.txt")
 
 annot <- read.delim("my.file.name.txt", header = FALSE, quote = "", stringsAsFactors = FALSE)
+annot <- apply(annot[,1:2], 1, paste0, collapse="|")
 
 # Loading package ape
 #install.packages("ape")
@@ -17,8 +18,8 @@ pdf("my.tree.pdf", pointsize=15, width=15, height=9)
 for(myfile in dir(path="analysis", pattern="\\.newick$", full.names=TRUE)){
  tre = read.tree(myfile)
 
- #x <- tre$tip.label[2]; TF <- regexpr(pattern=x, text=annot[,1]) > 0; sum(TF); annot[TF,3]
- tre$tip.label <- apply(as.matrix(tre$tip.label), MARGIN=c(1,2), function(x) paste0(annot[regexpr(x, annot[,1]) > 0, c(1,2,3)], collapse=" ") )
+ #x <- tre$tip.label[2]; TF <- regexpr(pattern=x, text=annot) > 0; sum(TF); annot[TF]
+ tre$tip.label <- apply(as.matrix(tre$tip.label), MARGIN=c(1,2), function(x) annot[regexpr(x, annot) > 0])
 
  #x <- tre$tip.label[1]; unlist(strsplit(annot[ grep(pattern=x, x=annot) ],":"))[2]
  #tre$tip.label = apply(as.matrix(tre$tip.label), MARGIN=c(1,2), function(x) unlist(strsplit(annot[ grep(pattern=x, x=annot) ],":"))[2] )
