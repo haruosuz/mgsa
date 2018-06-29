ec632387b9da240ee26d3182167c2d7aa49ed4b3
haruosuz <haruosuz@yahoo.co.jp>
2018-06-29 19:24:35 +0900

    update roary/scripts/my_roary_tree.R

--- roary/scripts/my_roary_tree.R
+++ roary/scripts/my_roary_tree.R
@@ -1,20 +1,19 @@
 # Set Working Directory
 #setwd("~/projects/mgsa/roary/2016-06-07") # i 95
-#setwd("~/projects/mgsa/roary/2016-06-21") # i 50
 
 system("find data/gbk -name '*.gbk' | xargs grep 'ORGANISM' | sort -u | grep -v 'phage' | sed 's/ ORGANISM  //; s#.*/##' | perl -pe 's/.gbk:/.gbk\t/;' > my.file.name.txt")
 annot <- read.delim("my.file.name.txt", header = FALSE, quote = "", stringsAsFactors = FALSE)
 annot <- apply(annot[,1:2], 1, paste0, collapse=" ")
 
-# Loading package ape
-#install.packages("ape")
-library(ape); #ls("package:ape")
+# Loading packages
+library(ape) #ls("package:ape") #install.packages("ape")
+library(phangorn) # midpoint
 
 # draw trees
 pdf("my.tree.pdf", pointsize=15, width=15, height=9)
 
 for(myfile in dir(path="analysis", pattern="\\.newick$", full.names=TRUE)){
- tre = read.tree(myfile)
+ tre <- read.tree(myfile)
 
  #x <- tre$tip.label[2]; TF <- regexpr(pattern=x, text=annot) > 0; sum(TF); annot[TF]
  tre$tip.label <- apply(as.matrix(tre$tip.label), MARGIN=c(1,2), function(x) annot[regexpr(x, annot) > 0])
@@ -22,10 +21,10 @@ for(myfile in dir(path="analysis", pattern="\\.newick$", full.names=TRUE)){
  write.tree(tre, file=paste0(myfile,".tre"))
 
  par(mfcol=c(1,1), mgp=c(1.7, 0.5, 0), mar=c(1, 0.5, 1, 0.8), cex=0.9) # mar=c(bottom, left, top, right)
- plot.phylo(tre, type="phylogram", use.edge.length=TRUE, show.node.label=TRUE, font=3)
- add.scale.bar(x=0.01, y=0.7)
+ #library(phangorn); tre <- midpoint(tre)
+ plot.phylo(ladderize(tre, right=FALSE), type="phylogram", use.edge.length=TRUE, show.node.label=TRUE, font=3)
+ add.scale.bar(x=0.01, y=0.9)#, col=2)
  legend("topleft", legend=myfile, box.lty=0)
 }
 
 dev.off()
-
